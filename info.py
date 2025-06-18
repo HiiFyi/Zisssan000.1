a8a80aeed3b89b9cc63fee78476985f1ad12522f
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-06-18 16:31:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,7 +72,7 @@ OWNER_USERNAME = environ.get('OWNER_USERNAME', 'Arpitbotmovies') # owner usernam
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/filmsexpert2')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/filmsexpert2')
+CHNL_LNK = environ.get('CHNL_LNK', 'http://t.me/stark_network07')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/Stark_Network07') # Support Chat Link Without https:// or @
 
